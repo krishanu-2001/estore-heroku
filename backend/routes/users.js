e25a39a4cedb6c2de9ffa6bae2f39e2df353ae50
Harshit <harshitdiyundi@gmail.com>
2020-08-06 13:50:56 +0530

    Backend organised

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,19 +1,11 @@
 const router = require('express').Router();
-let User = require('../models/user.model.js');
+const objectID = require('mongoose').Types.ObjectId;
+
+const User = require('../models/user.model');
 
 router.route('/').get((req, res) => {
     User.find()
-        .then(users => res.json(users))
-        .catch(err => res.status(400).json('Error: ' + err));
-});
-
-router.route('/add').post((req,res) => {
-    const username = req.body.username;
-
-    const newUser = new User({username});
-
-    newUser.save()
-        .then(() => res.json('User Added!'))
+        .then(items => res.json(items))
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
