597fa5fcd4c0a2aeab66980909f5ba05b3454d71
KRISHANU SAINI <56930593+krishanu-2001@users.noreply.github.com>
2020-08-10 12:43:57 +0530

    Revert "upd local"

--- src/Components/Sign_up_modal.js
+++ src/Components/Sign_up_modal.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import './Comp-CSS/SignInUp_modal.css';
 import './Comp-CSS/Sign_Up_form.css';
-import axios from "axios";
 
 const Modal = React.forwardRef((props, ref)=>{
 
@@ -22,52 +21,24 @@ const Modal = React.forwardRef((props, ref)=>{
     const close = ()=>{
     setDisplay(false);
     };
-
-    const [email,setEmail] = React.useState("");
-    const [password,setPass] = React.useState("");
-    const [cpassword,setCpass] = React.useState("");
-
-    const signUpHandler = (e)=>{
-        e.preventDefault();
-        if(email !== "" && password !== "" && cpassword !=="" && password === cpassword)
-        {
-            axios.post('http://localhost:5000/users/new',
-            {
-                "username": email,
-                "password": password
-            }
-            ,{headers: {'Accept': 'application/json'}})
-            .then((res)=>{
-                console.log(res);
-            })
-            .catch((err)=>{
-                console.log(err);
-            })
-        }
-        else
-        {
-            console.log("Wrong Credentials");
-        }
-        close();
-    }
     
     if(display)
     {
         return ReactDOM.createPortal(
             <div className = {"modal-wrapper"}>
-                <div className={"modal-backdrop"}>
+                <div onClick={close} className={"modal-backdrop"}>
                     <div className={"modal-box"}>
-                       <form onSubmit={signUpHandler}>
+                       <form>
                         <div className="form-container">
                             <h1>Sign Up</h1><br/>
                             <p>Please fill in this form to create an account.</p>
                              <hr/><br/>
                             <label><b>Email</b></label><br/>
-                            <input onChange={e => setEmail(e.target.value)} type="text" placeholder="Enter Email" required/><br/>
+                            <input type="text" placeholder="Enter Email" required/><br/>
                             <label><b>Password</b></label><br/>
-                            <input onChange={e => setPass(e.target.value)} type="password" placeholder="Enter Password" required/><br/>
+                            <input type="password" placeholder="Enter Password" required/><br/>
                             <label><b>Confirm Password</b></label><br/>
-                            <input onChange={e => setCpass(e.target.value)} type="password" placeholder="Repeat Password" required/>
+                            <input type="password" placeholder="Repeat Password" required/>
 
                     <div className="clearfix">
                         <button type="button" class="cancelbtn" onClick={close}>Cancel</button>
