dc47952ab0a5ddca0bbcbdc200ba19fd9d6a5d83
lokesh-singla <67456307+lokesh-singla@users.noreply.github.com>
2020-08-25 00:53:52 +0530

    Update Individual-item.js

--- src/Components/Individual-item.js
+++ src/Components/Individual-item.js
@@ -1,20 +1,80 @@
-
+import axios from 'axios';
+import {Link} from 'react-router-dom';
+import Cookies from 'js-cookie';
+import './Comp-CSS/item-card.css';
 import React, { Component } from 'react';
 import './Comp-CSS/individual-item.css';
+
+var prices;
 
 export class ItemHtml extends React.Component {
   constructor(props) {
     super(props);
+
+    this.onChangeQuantity=this.onChangeQuantity.bind(this);
+    this.onClick=this.addToBasket.bind(this);
+
+    this.state={
+      quantity: 0,
+}
   }
 
-  state ={
-    name:"null",
-  }
 
   getVAl(){
     var token = "Potato";
     return token;
   }
+
+  componentDidMount(){
+    axios.get('http://localhost:5000/items/')
+    .then(response=>{
+      console.log(response);
+      response.data.map(curr=>{
+        if(curr.itemname===this.props.match.params.id)
+        {
+          prices=curr.price;
+        }
+      })
+    }
+    )
+  }
+  
+
+  onChangeQuantity(e){
+    this.setState({
+      quantity: e.target.valueAsNumber
+    });
+  }
+
+  addToBasket(e){
+    console.log(this.props);
+    e.preventDefault();
+    
+      const newBasketItem={
+        itemname: this.props.match.params.id,
+        price: prices,
+        quantity: this.state.quantity
+      }
+  console.log(newBasketItem);
+
+    if(newBasketItem.quantity !== 0)
+    {
+      axios.post('http://localhost:5000/basket/add',{
+      newBasketItem,
+    },
+    {headers: {
+      'x-auth-token': Cookies.get('token')
+    }})
+    .then(res=> {
+        console.log(res.data);
+        Cookies.set('basket', JSON.stringify(res.data.basket));
+        console.log('Cookies Set', Cookies.get('basket'));
+    })
+    .catch((err)=>{
+      console.log(err);
+  })
+    }
+}
 
   render() {
     var name = this.props.match.params.id;
@@ -77,8 +137,8 @@ export class ItemHtml extends React.Component {
                         <div classNameName="myForm1">
                           <div>
                               <br></br>
-                              <form style={{border:"0px"}}>
-                                  <input type="number" min="0" placeholder="0" name="quantity" style={{"width":"60%", }}/>
+                              <form style={{border:"0px"}} onSubmit={this.onClick}>
+                                  <input type="number" min="0" placeholder="0" name="quantity" style={{"width":"60%", }} onChange={this.onChangeQuantity }/>
                                   <input type="hidden" name="itemName" value={name}/>
                                   <input type="submit" value="add to cart" name="submit" />
                               </form>
